adeb036108415f6690199bb84d54f8797d20f2da
JYeonKim <jyeonk.kor@gmail.com>
2022-01-14 21:54:35 +0900

    simple jwt 추가
    Co-authored-by: leejb-se <leejb-se@users.noreply.github.com>

--- seggle/settings.py
+++ seggle/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
+from django.conf import settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,12 +42,14 @@ INSTALLED_APPS = [
 
     # DRF
     'rest_framework',
+    # simple jwt
+    'rest_framework_simplejwt',
 
     # ckeditor
     'ckeditor',
     'ckeditor_uploader'
 
-    # add app
+    # my app
     'account',
     'announcement',
     'classes',
@@ -144,7 +148,47 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # auth 유저 모델은 account의 User를 쓰겠다는 의미
 AUTH_USER_MODEL = 'account.User'
 
-# ckeditor 업로드 폴더 설정
-CKEDITOR_UPLOAD_PATH = 'uploads/'
-MEDIA_URL = 'media/'
-MEDIA_ROOT = 'media/'
+# # ckeditor 업로드 폴더 설정
+# CKEDITOR_UPLOAD_PATH = 'uploads/'
+# MEDIA_URL = 'media/'
+# MEDIA_ROOT = 'media/'
+
+# rest_framework token 관련해서 추가
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+# simple jwt
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': settings.SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
