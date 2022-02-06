3358f8b664b8663013bb433f427c77083c2cae44
JYeonKim <juyeonk.kor@gmail.com>
2022-02-06 15:05:51 +0900

    modify createsuperuser

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User
+# from django.contrib.auth.admin import UserAdmin
+from . import models
 
-admin.site.register(User, UserAdmin)
+# admin.site.register(User, UserAdmin)
+
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'username',
+        'email',
+        'name',
+        'privilege',
+        'date_joined',
+    )
+
+    list_display_links = (
+        'username',
+        'email',
+    )
