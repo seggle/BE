6cb2b75a7376bfa29bc3dc89fd337ba8408c51a4
Donghyeon Cho <hyeon9698@naver.com>
2022-03-31 23:21:38 +0900

    remove csv, ipynb field from leaderboard

--- leaderboard/serializers.py
+++ leaderboard/serializers.py
@@ -5,21 +5,17 @@ from submission.models import SubmissionClass, SubmissionCompetition
 class LeaderboardClassSerializer(serializers.ModelSerializer):
     name = serializers.CharField()
     score = serializers.FloatField()
-    csv = serializers.CharField()
-    ipynb = serializers.CharField()
     submission_id = serializers.IntegerField()
 
     class Meta:
         model = SubmissionClass
-        fields = ["id", "username", "name", "score", "created_time", "csv", "ipynb", "submission_id"]
+        fields = ["id", "username", "name", "score", "created_time", "submission_id"]
 
 class LeaderboardCompetitionSerializer(serializers.ModelSerializer):
     name = serializers.CharField()
     score = serializers.FloatField()
-    csv = serializers.CharField()
-    ipynb = serializers.CharField()
     submission_id = serializers.IntegerField()
 
     class Meta:
         model = SubmissionCompetition
-        fields = ["id", "username", "name", "score", "created_time", "csv", "ipynb", "submission_id"]
+        fields = ["id", "username", "name", "score", "created_time", "submission_id"]
