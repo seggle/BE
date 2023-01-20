6bf0bf505d48cfec748fcc1ee1ac4c45e6ffc3eb
Capella87 <contact@capelladev.com>
2023-01-20 18:57:59 +0900

    Fix the order of Class model in models.py

--- classes/models.py
+++ classes/models.py
@@ -3,6 +3,7 @@ from account.models import User
 
 # Create your models here.
 
+
 class ActiveModelQuerySet(models.QuerySet):
 
     def not_active(self):
@@ -11,8 +12,25 @@ class ActiveModelQuerySet(models.QuerySet):
     def active(self):
         return self.filter(is_deleted=False)
 
+
+class Class(models.Model):
+    name = models.TextField()
+    year = models.IntegerField()
+    semester = models.IntegerField()
+    created_user = models.ForeignKey(User, on_delete=models.CASCADE, db_column="created_user",
+                                     related_name='created_user', to_field="username")
+    is_deleted = models.BooleanField(default=False)
+    objects = ActiveModelQuerySet().as_manager()
+
+    def __str__(self):
+        return self.id
+
+    class Meta:
+        db_table = "class"
+
+
 class ClassUser(models.Model):
-    class_id = models.ForeignKey('Class', on_delete=models.CASCADE, db_column="class_id")
+    class_id = models.ForeignKey(Class, on_delete=models.CASCADE, db_column="class_id")
     username = models.ForeignKey(User, on_delete=models.CASCADE, db_column="username", to_field="username")
     is_show = models.BooleanField(default=True)
     privilege = models.IntegerField()
@@ -22,17 +40,3 @@ class ClassUser(models.Model):
 
     class Meta:
         db_table = "class_user"
-
-class Class(models.Model):
-    name = models.TextField()
-    year = models.IntegerField()
-    semester = models.IntegerField()
-    created_user = models.ForeignKey(User, on_delete=models.CASCADE, db_column="created_user", related_name='created_user', to_field="username")
-    is_deleted = models.BooleanField(default=False)
-    objects = ActiveModelQuerySet().as_manager()
-    
-    def __str__(self):
-        return self.id
-
-    class Meta:
-        db_table = "class"
