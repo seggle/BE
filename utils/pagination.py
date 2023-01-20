08c3c900870fdcc7247db3b0972a55af2c733c6f
Capella87 <contact@capelladev.com>
2023-01-20 18:58:35 +0900

    Implement a pagination for list
    Source: https://stackoverflow.com/questions/38284440/drf-pagination-without-queryset

--- utils/pagination.py
+++ utils/pagination.py
@@ -1,4 +1,10 @@
+from django.db.models import QuerySet
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.request import Request
+from collections import OrderedDict
+from django.core.paginator import Paginator
+from django.http.response import JsonResponse
+from rest_framework.response import Response
 
 
 class PaginationHandlerMixin(object):
@@ -27,3 +33,34 @@ class PaginationHandlerMixin(object):
 
 class BasicPagination(PageNumberPagination):
     page_size_query_param = 'limit'
+
+
+# Pagination for list
+# https://stackoverflow.com/questions/38284440/drf-pagination-without-queryset
+class ListPagination:
+    def __init__(self, request: Request):
+        self._url_scheme = request.scheme
+        self._host = request.get_host()
+        self._path_info = request.path_info
+
+    def paginate_list(self, data: list, page_size: int, page_number: int) -> JsonResponse:
+        paginator = Paginator(data, page_size)
+        page = paginator.page(page_number)
+
+        previous_url = None
+        next_url = None
+        if self._host and self._path_info:
+            if page.has_previous():
+                previous_url = '{}://{}{}?limit={}&page={}'.format(self._url_scheme, self._host, self._path_info,
+                                                                   page_size, page.previous_page_number())
+            if page.has_next():
+                next_url = '{}://{}{}?limit={}&page={}'.format(self._url_scheme, self._host, self._path_info, page_size,
+                                                               page.next_page_number())
+        response_dict = OrderedDict([
+            ('count', len(data)),
+            ('next', next_url),
+            ('previous', previous_url),
+            ('results', page.object_list)
+        ])
+
+        return JsonResponse(response_dict, status=200, safe=False)
