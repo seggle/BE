bf668408fab5088fd841ee8b9081d1b5ba75969e
LeeDayday <dayoon0905@gmail.com>
2023-02-06 23:06:27 +0900

    CHORE: update tasks.py just for test

--- reminder/tasks.py
+++ reminder/tasks.py
@@ -1,2 +1,11 @@
 # Create your tasks here
+from __future__ import absolute_import, unicode_literals
+
+import datetime
 from celery import shared_task
+from seggle.celery import app
+
+# test 용 함수
+@shared_task
+def printTime():
+    print("Testtime: ", datetime.now())
