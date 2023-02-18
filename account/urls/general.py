102f9444ab77b31c30485d8996ef4e54691b0647
LeeDayday <dayoon0905@gmail.com>
2023-02-18 15:51:38 +0900

    CHORE: Fix view arguments related to simple-jwt

--- account/urls/general.py
+++ account/urls/general.py
@@ -1,11 +1,8 @@
 from tokenize import Token
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from account.views.general import (
     LogoutView, UserCompetitionInfoView, UserRegisterView,
-    UserInfoView, ClassInfoView, RefreshView, ContributionsView, UserClassPrivilege, UserCompetitionPrivilege,
-    TokenObtainResultView
-)
+    UserInfoView, ClassInfoView, ContributionsView, UserClassPrivilege, UserCompetitionPrivilege)
 
 
 #simple-jwt
@@ -18,8 +15,8 @@ from account.serializers import TokenObtainResultSerializer
 app_name = "account"
 urlpatterns = [
     path('', UserRegisterView.as_view()),
-    path('login/', TokenObtainResultView.as_view(), name='token_obtain_result'),
-    path('login/refresh/', RefreshView.as_view(), name='token_refresh'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_result'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('logout/', LogoutView.as_view(), name='auth_logout'),
     path('class/', ClassInfoView.as_view()),
     path('class/<int:class_id>/', UserClassPrivilege.as_view(), name="user_class_privilege"),
