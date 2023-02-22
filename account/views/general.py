2ee2b82802f314f67f0ddcb7a932aaf4f0a56567
LeeDayday <dayoon0905@gmail.com>
2023-02-22 18:47:22 +0900

    Fix cookie max_age in CookieTokenRefreshView and commented out code related to CFRS

--- account/views/general.py
+++ account/views/general.py
@@ -353,7 +353,7 @@ class CookieTokenRefreshView(TokenRefreshView):
         response.set_cookie(
             key=settings.SIMPLE_JWT['AUTH_COOKIE'],
             value=response.data.get('access'),
-            expires=settings.SIMPLE_JWT['ACCESS_TOKEN_LIFETIME'],
+            max_age=3600,  # 1 hour
             secure=settings.SIMPLE_JWT['AUTH_COOKIE_SECURE'],
             httponly=settings.SIMPLE_JWT['AUTH_COOKIE_HTTP_ONLY'],
             samesite=settings.SIMPLE_JWT['AUTH_COOKIE_SAMESITE']
