c10411c4710562c6f80c480576ff06f4c6afc474
LeeDayday <dayoon0905@gmail.com>
2023-02-24 16:19:13 +0900

    Add code for Token Refresh in CustomAuthentication

--- account/authenticate.py
+++ account/authenticate.py
@@ -1,9 +1,10 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.serializers import TokenRefreshSerializer
 from django.conf import settings
+from django.http import HttpResponse
 
-from rest_framework.authentication import CSRFCheck, TokenAuthentication
+from rest_framework.authentication import CSRFCheck
 from rest_framework import exceptions
-
 
 def enforce_csrf(request):
     check = CSRFCheck()
@@ -16,14 +17,30 @@ def enforce_csrf(request):
 class CustomAuthentication(JWTAuthentication):
     def authenticate(self, request):
         header = self.get_header(request)
-
+        print(header)
         if header is None:
             raw_token = request.COOKIES.get(settings.SIMPLE_JWT['AUTH_COOKIE']) or None
         else:
             raw_token = self.get_raw_token(header)
+        # if access_token expires
         if raw_token is None:
-            return None
+            refresh_token = request.COOKIES.get(settings.SIMPLE_JWT['AUTH_COOKIE_REFRESH']) or None
+            serializer = TokenRefreshSerializer(data={'refresh': refresh_token})
+            # if refresh_token expires
+
+            # if refresh_token exists(=valid)
+            if serializer.is_valid():
+                raw_token = serializer.validated_data['access']
+                response = HttpResponse()
+                response.set_cookie(
+                    key=settings.SIMPLE_JWT['AUTH_COOKIE'],
+                    value=raw_token,
+                    max_age=60,  # 1 hour
+                    secure=settings.SIMPLE_JWT['AUTH_COOKIE_SECURE'],
+                    httponly=settings.SIMPLE_JWT['AUTH_COOKIE_HTTP_ONLY'],
+                    samesite=settings.SIMPLE_JWT['AUTH_COOKIE_SAMESITE']
+                )
 
         validated_token = self.get_validated_token(raw_token)
-        enforce_csrf(request)
+        # enforce_csrf(request)
         return self.get_user(validated_token), validated_token
