8c9447b718d4a36955c5bfad0f3217becf4d1441
LeeDayday <dayoon0905@gmail.com>
2023-03-04 00:28:37 +0900

    Delete refreshing process in authenticate

--- account/authenticate.py
+++ account/authenticate.py
@@ -1,46 +1,27 @@
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework_simplejwt.serializers import TokenRefreshSerializer
+from rest_framework_simplejwt import authentication as jwt_authentication
 from django.conf import settings
-from django.http import HttpResponse
+from rest_framework import authentication, exceptions as rest_exceptions
 
-from rest_framework.authentication import CSRFCheck
-from rest_framework import exceptions
 
 def enforce_csrf(request):
-    check = CSRFCheck()
-    check.process_request(request)
+    check = authentication.CSRFCheck(request)
     reason = check.process_view(request, None, (), {})
     if reason:
-        raise exceptions.PermissionDenied('CSRF Failed: %s' % reason)
+        raise rest_exceptions.PermissionDenied('CSRF Failed: %s' % reason)
 
 
-class CustomAuthentication(JWTAuthentication):
+class CustomAuthentication(jwt_authentication.JWTAuthentication):
     def authenticate(self, request):
         header = self.get_header(request)
-        print(header)
+
         if header is None:
             raw_token = request.COOKIES.get(settings.SIMPLE_JWT['AUTH_COOKIE']) or None
         else:
             raw_token = self.get_raw_token(header)
-        # if access_token expires
-        if raw_token is None:
-            refresh_token = request.COOKIES.get(settings.SIMPLE_JWT['AUTH_COOKIE_REFRESH']) or None
-            serializer = TokenRefreshSerializer(data={'refresh': refresh_token})
-            # if refresh_token expires
 
-            # if refresh_token exists(=valid)
-            if serializer.is_valid():
-                raw_token = serializer.validated_data['access']
-                response = HttpResponse()
-                response.set_cookie(
-                    key=settings.SIMPLE_JWT['AUTH_COOKIE'],
-                    value=raw_token,
-                    max_age=60,  # 1 hour
-                    secure=settings.SIMPLE_JWT['AUTH_COOKIE_SECURE'],
-                    httponly=settings.SIMPLE_JWT['AUTH_COOKIE_HTTP_ONLY'],
-                    samesite=settings.SIMPLE_JWT['AUTH_COOKIE_SAMESITE']
-                )
+        if raw_token is None:
+            return None
 
         validated_token = self.get_validated_token(raw_token)
-        # enforce_csrf(request)
+        enforce_csrf(request)
         return self.get_user(validated_token), validated_token
